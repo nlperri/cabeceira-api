bccf432b3609aea43a93164a18a2440bf531c80d
natalia <lnataliaperri@gmail.com>
2023-11-07 10:18:18 -0300

    first commit

---
+++ src/main/java/cabeceira/api/CabeceiraApiApplication.java
@@ -0,0 +1,13 @@
+package cabeceira.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeceiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabeceiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cabeceira/api/CabeceiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cabeceira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeceiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

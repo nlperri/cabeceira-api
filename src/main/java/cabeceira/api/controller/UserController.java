c32353c66733faca967d05ec82cb9082f0995f2b
Pedro Samuel <cardev604@gmail.com>
2023-11-08 19:50:22 -0300

    Cadastro usuário e autenticação. Closes #7
    Co-authored-by: João Vìtor <JoaoVitorSantDrade@users.noreply.github.com>
    Co-authored-by: Natália Perri <nlperri@users.noreply.github.com>
    Co-authored-by: Pedro Cecílio <Pedro-Cecilio@users.noreply.github.com>
    Co-authored-by: Vinícius <viny9@users.noreply.github.com>

--- src/main/java/cabeceira/api/controller/UserController.java
+++ src/main/java/cabeceira/api/controller/UserController.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cabeceira.api.domain.user.CreateUserDTO;
 import cabeceira.api.domain.user.UserService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
--- src/main/java/cabeceira/api/domain/user/CreateUserDTO.java
+++ src/main/java/cabeceira/api/domain/user/CreateUserDTO.java
@@ -1,12 +1,18 @@
 package cabeceira.api.domain.user;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
 
 public record CreateUserDTO(
-                @NotBlank String name,
-                @NotBlank String lastName,
-                @NotBlank @Email String email,
-                @NotBlank String password) {
-
+    @NotBlank @Email 
+    String email,
+    @NotBlank @Size(min = 5)
+    String password,
+    @NotBlank
+    String name,
+    @NotBlank
+    String lastName
+    ) {
+    
 }

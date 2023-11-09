c0068d64b2bf234cc77cea4be5c23e0e56967d01
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2023-11-09 00:04:31 -0300

    Update

--- src/main/java/cabeceira/api/domain/user/UserRepository.java
+++ src/main/java/cabeceira/api/domain/user/UserRepository.java
@@ -1,7 +1,9 @@
 package cabeceira.api.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
 }

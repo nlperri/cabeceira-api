a16e24de4ac889a4ba1e193c5d91b3b0d8025032
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2023-11-10 00:56:44 -0300

    Uso de @JsonProperty em UpdateUserBooksDTO #28

--- src/main/java/cabeceira/api/domain/userBooks/dto/UpdateUserBooksDTO.java
+++ src/main/java/cabeceira/api/domain/userBooks/dto/UpdateUserBooksDTO.java
@@ -1,8 +1,12 @@
 package cabeceira.api.domain.userBooks.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import cabeceira.api.domain.userBooks.BookshelfStatus;
 
 public record UpdateUserBooksDTO(
+    @JsonProperty("bookshelfStatus")
     BookshelfStatus bookshelfStatus,
+    @JsonProperty("readedPages")
     Integer readedPages
 ) { }

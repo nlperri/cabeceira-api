5abb133bc9885deb1af4bb9b2b5e87eb9ee5dd4e
Pedro Samuel <cardev604@gmail.com>
2023-11-13 18:10:17 -0300

    Adicionando testes unitários na classe UserBooksService
    Closes #53
    
    Co-authored-by: João Vìtor <JoaoVitorSantDrade@users.noreply.github.com>
    Co-authored-by: Natália Perri <nlperri@users.noreply.github.com>
    Co-authored-by: Pedro Cecílio <Pedro-Cecilio@users.noreply.github.com>
    Co-authored-by: Vinícius <viny9@users.noreply.github.com>

--- src/test/java/cabeceira/api/domain/mocks/UserBookMock.java
+++ src/test/java/cabeceira/api/domain/mocks/UserBookMock.java
@@ -9,4 +9,8 @@ public class UserBookMock {
     public static UserBooks create(User user, Book book) {
         return new UserBooks(user, book, BookshelfStatus.WANT_TO_READ, 0);
     }
+
+    public static UserBooks create(User user, Book book, String id) {
+        return new UserBooks(id ,user, book, BookshelfStatus.WANT_TO_READ, 0);
+    }
 }

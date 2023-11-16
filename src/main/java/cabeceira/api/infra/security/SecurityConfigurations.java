ac7bd5f6151a7f10b8d13270e0e8f121561e7791
natalia <lnataliaperri@gmail.com>
2023-11-16 14:55:49 -0300

    Correção erro de CORS (bookshelf) Closes #65

--- src/main/java/cabeceira/api/infra/security/SecurityConfigurations.java
+++ src/main/java/cabeceira/api/infra/security/SecurityConfigurations.java
@@ -26,6 +26,7 @@ public class SecurityConfigurations {
                 .csrf(csfr -> csfr.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/users").permitAll()
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
